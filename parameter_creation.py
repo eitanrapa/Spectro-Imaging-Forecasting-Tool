cc6164c0200d9710dac7226fea7b9cf79c44d914
Eitan Rapaport <eitanrapa@gmail.com>
2024-03-01 21:56:35 +0000

    Structural changes

--- parameter_creation.py
+++ parameter_creation.py
@@ -1,29 +1,124 @@
 import csv
 import numpy as np
-from astropy.io import fits
+import camb
 
-#Read FITS file
-fname = '/bolocam/bolocam/erapaport/Auxiliary/cmb_3arcmin.fits'
-hdu = fits.open(fname)
-image_data = hdu[1].data
+seed = 583
+np.random.seed(seed)
 
-params = [[0,0,0,0,0]]
-params = np.asarray(params)
-        
-for i in range(100):
-    sides_long = np.random.randint(0,160)
-    sides_lat = np.random.randint(0,160)
-    cmb_long = np.random.randint(2,197)
-    cmb_lat = np.random.randint(2,197)
-    amp_cmb = image_data[cmb_long][cmb_lat] - np.mean(image_data[cmb_long-2:cmb_long+3,cmb_lat-2:cmb_lat+3])
-    amp_ksz = np.random.normal(0,6e-7)
-    amp_tsz = np.random.normal(0,6e-7)
-    params_real = [[sides_long, sides_lat, amp_cmb, amp_ksz, amp_tsz]]
-    params = np.append(params, params_real,axis=0)
-    
-params = params[1:,:]
 
-#Write simulation output, change directory/name
-with open('/bolocam/bolocam/erapaport/Auxiliary/parameter_file_100', 'w') as f:
-    write = csv.writer(f)
-    write.writerows(params)
+def create_cmb_map(resolution=3.0):
+    """
+
+    """
+
+    # Set up a new set of parameters for CAMB
+    pars = camb.CAMBparams()
+
+    # This function sets up with one massive neutrino and helium set using BBN consistency
+    pars.set_cosmology(H0=67.5, ombh2=0.022, omch2=0.122, mnu=0.06, omk=0, tau=0.06)
+    pars.InitPower.set_params(As=2e-9, ns=0.965, r=0)
+    lmax = 5000
+    pars.set_for_lmax(lmax, lens_potential_accuracy=0)
+
+    # calculate results for these parameters
+    results = camb.get_results(pars)
+
+    # get dictionary of CAMB power spectra
+    powers = results.get_cmb_power_spectra(pars, CMB_unit='K')
+    totCL = powers['total']
+    DlTT = totCL[:, 0]
+    ell = np.arange(totCL.shape[0])
+
+    # variables to set up the size of the map
+    N = 2 ** 10  # this is the number of pixels in a linear dimension
+
+    # since we are using lots of FFTs this should be a factor of 2^N
+    pix_size = resolution  # size of a pixel in arcminutes
+
+    # convert Dl to Cl
+    ClTT = DlTT * 2 * np.pi / (ell * (ell + 1.))
+    ClTT[0] = 0.  # set the monopole and the dipole of the Cl spectrum to zero
+    ClTT[1] = 0.
+
+    # make a 2D real space coordinate system
+    onesvec = np.ones(N)
+    inds = (np.arange(N) + .5 - N / 2.) / (N - 1.)  # create an array of size N between -0.5 and +0.5
+
+    # compute the outer product matrix: X[i, j] = onesvec[i] * inds[j] for i,j
+    # in range(N), which is just N rows copies of inds - for the x dimension
+    X = np.outer(onesvec, inds)
+
+    # compute the transpose for the y dimension
+    Y = np.transpose(X)
+
+    # radial component R
+    R = np.sqrt(X ** 2. + Y ** 2.)
+
+    # now make a 2D CMB power spectrum
+    pix_to_rad = (
+            pix_size / 60. * np.pi / 180.)  # going from pix_size in arcmins to degrees and then degrees to radians
+    ell_scale_factor = 2. * np.pi / pix_to_rad  # now relating the angular size in radians to multipoles
+    ell2d = R * ell_scale_factor  # making a fourier space analogue to the real space R vector
+    ClTT_expanded = np.zeros(int(ell2d.max()) + 1)
+
+    # making an expanded Cl spectrum (of zeros) that goes all the way to the size of the 2D ell vector
+    ClTT_expanded[0:ClTT.size] = ClTT  # fill in the Cls until the max of the ClTT vector
+
+    # the 2D Cl spectrum is defined on the multiple vector set by the pixel scale
+    CLTT2d = ClTT_expanded[ell2d.astype(int)]
+
+    # now make a realization of the CMB with the given power spectrum in real space
+    random_array_for_T = np.random.normal(0, 1, (N, N))
+    FT_random_array_for_T = np.fft.fft2(random_array_for_T)  # take FFT since we are in Fourier space
+
+    FT_2d = np.sqrt(CLTT2d) * FT_random_array_for_T  # we take the sqrt since the power spectrum is T^2
+
+    # move back from ell space to real space
+    CMB_T = np.fft.ifft2(np.fft.fftshift(FT_2d))
+    # move back to pixel space for the map
+    CMB_T = CMB_T / (pix_size / 60. * np.pi / 180.)
+    # we only want to plot the real component
+    CMB_T = np.real(CMB_T)
+
+    return CMB_T
+
+
+def create_parameter_file(cmb_resolution, file_name):
+    """
+
+    """
+
+    params = [[0, 0, 0, 0, 0]]
+    params = np.asarray(params)
+
+    # Create CMB map
+    CMB_T = create_cmb_map(cmb_resolution)
+
+    for i in range(500):  # Make 500 realizations of parameters
+
+        # Pick coordinates of SIDES continuum
+        sides_long = np.random.randint(0, 160)
+        sides_lat = np.random.randint(0, 160)
+
+        # Pick coordinates of CMB map
+        cmb_long = np.random.randint(2, 1000)
+        cmb_lat = np.random.randint(2, 1000)
+
+        # Inpainting of 5x5 3 arcmin pixels
+        amp_cmb = CMB_T[cmb_long, cmb_lat] * 1e-6
+        amp_cmb = amp_cmb - np.mean(CMB_T[cmb_long - 2:cmb_long + 3, cmb_lat - 2:cmb_lat + 3])
+
+        # Make CMB secondary anisotropies
+        amp_ksz = np.random.normal(0, 6e-7)
+        amp_tsz = np.random.normal(0, 6e-7)
+
+        params_realization = [[sides_long, sides_lat, amp_cmb, amp_ksz, amp_tsz]]
+        params = np.append(params, params_realization, axis=0)
+
+    params = params[1:, :]
+
+    # Write simulation output, change directory/name
+
+    with open('/bolocam/bolocam/erapaport/Auxiliary/' + file_name, 'w') as f:
+        write = csv.writer(f)
+        write.writerows(params)
--- projections.py
+++ projections.py
@@ -1,6 +1,5 @@
 import SIFT_classes as SIFT
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import cm
 import corner
 import pygtc
 import numpy as np
@@ -10,122 +9,143 @@ import pandas as pd
 Cosmological terms from G. Sun
 '''
 
-c = 299792458.0                                         # Speed of light - [c] = m/s
-h_p = 6.626068e-34                                      # Planck's constant in SI units
-k_b = 1.38065e-23                                       # Boltzmann constant in SI units
-MJyperSrtoSI = 1e-20                                    # MegaJansky/Sr to SI units
-GHztoHz = 1e9                                           # Gigahertz to hertz
-HztoGHz = 1e-9                                          # Hertz to Gigahertz
-TCMB = 2.725                                            # Canonical CMB in Kelvin
-m = 9.109*10**(-31)                                     # Electron mass in kgs
+c = 299792458.0  # Speed of light - [c] = m/s
+h_p = 6.626068e-34  # Planck's constant in SI units
+k_b = 1.38065e-23  # Boltzmann constant in SI units
+MJyperSrtoSI = 1e-20  # MegaJansky/Sr to SI units
+GHztoHz = 1e9  # Gigahertz to hertz
+HztoGHz = 1e-9  # Hertz to Gigahertz
+TCMB = 2.725  # Canonical CMB in Kelvin
+m = 9.109 * 10 ** (-31)  # Electron mass in kgs
 
-#Function to plot the spectra and OLIMPO, SOFTS bands
-def Differential_Intensity_Projection(y_value, electron_temperature, peculiar_velocity, sides_long, sides_lat, bands, time):
-    labels = ('tau','temperature','peculiar_velocity','a_sides','b_sides')
-    freq = np.linspace(80e9,1000e9,2000)
-    
-    sz_template = SIFT.szpack_signal(freq, SIFT.y_to_tau(y_value, electron_temperature), electron_temperature, peculiar_velocity)
-    
+
+# Function to plot the spectra and OLIMPO, SOFTS bands
+def differential_intensity_projection(y_value, electron_temperature, peculiar_velocity, sides_long, sides_lat, bands,
+                                      time):
+
+    freq = np.linspace(80e9, 1000e9, 2000)
+
+    sz_template = SIFT.szpack_signal(freq, SIFT.y_to_tau(y_value, electron_temperature), electron_temperature,
+                                     peculiar_velocity)
+
     sides_template = SIFT.SIDES_continuum(freq, sides_long, sides_lat)
-    
-    plt.rc('xtick',labelsize=18)
-    plt.rc('ytick',labelsize=18)
-    fig = plt.figure(figsize=(10,10))
-  
-    #Plot SZ components
-    plt.plot(freq*HztoGHz,abs(sz_template),'--k',label='Total SZ',linewidth=2)
-    plt.plot(freq*HztoGHz,abs(SIFT.szpack_signal(freq, SIFT.y_to_tau(y_value, electron_temperature), electron_temperature, 1e-11) - SIFT.classical_tsz(y_value, freq)), label='rSZ ' + str(electron_temperature) + ' keV')
-        
-    plt.plot(freq*HztoGHz,abs(SIFT.classical_tsz(y_value,freq)),label='tSZ y=' + str(y_value))
 
-    #SIDES continuum model and emission line data
-    plt.plot(freq*HztoGHz,abs(sides_template),color='pink',label='SIDES continuum')
-    
-        
+    plt.rc('xtick', labelsize=18)
+    plt.rc('ytick', labelsize=18)
+
+    # Plot SZ components
+    plt.plot(freq * HztoGHz, abs(sz_template), '--k', label='Total SZ', linewidth=2)
+    plt.plot(freq * HztoGHz,
+             abs(SIFT.szpack_signal(freq, SIFT.y_to_tau(y_value, electron_temperature), electron_temperature,
+                                    1e-11) - SIFT.classical_tsz(y_value, freq)),
+             label='rSZ ' + str(electron_temperature) + ' keV')
+
+    plt.plot(freq * HztoGHz, abs(SIFT.classical_tsz(y_value, freq)), label='tSZ y=' + str(y_value))
+
+    # SIDES continuum model and emission line data
+    plt.plot(freq * HztoGHz, abs(sides_template), color='pink', label='SIDES continuum')
+
     nu_total_array = np.empty(0)
-    total_sz_array = np.empty(0)
     sigma_b_array = np.empty(0)
-    
+
     for band in bands:
-        if (band['type'] == 'OLIMPO'):
-            #80 hour normalized
-            rms_value = band['rms']*(np.sqrt(80)/(np.sqrt(time/3600)))
-            nu_vec_b = band['nu_meanGHz']*GHztoHz
-            x = h_p*nu_vec_b/(k_b*TCMB)
-            sigma_B_b = 2*k_b*((nu_vec_b/c)**2)*(x/(np.exp(x)-1))*(x*np.exp(x))/(np.exp(x)-1)*band['rms']*1e-6
+        if band['type'] == 'OLIMPO':
+
+            # 80 hour normalized
+            rms_value = band['rms'] * (np.sqrt(80) / (np.sqrt(time / 3600)))
+            nu_vec_b = band['nu_meanGHz'] * GHztoHz
+            x = h_p * nu_vec_b / (k_b * TCMB)
+            sigma_B_b = 2 * k_b * ((nu_vec_b / c) ** 2) * (x / (np.exp(x) - 1)) * (x * np.exp(x)) / (
+                    np.exp(x) - 1) * band['rms'] * 1e-6
             nu_total_array = np.concatenate((nu_total_array, nu_vec_b), axis=None)
             sigma_b_array = np.concatenate((sigma_b_array, sigma_B_b), axis=None)
-        if (band['type'] == 'spectrometric'):
+
+        if band['type'] == 'spectrometric':
             nu_vec_b, sigma_B_b = SIFT.sigB(band, time)
             nu_total_array = np.concatenate((nu_total_array, nu_vec_b), axis=None)
             sigma_b_array = np.concatenate((sigma_b_array, sigma_B_b), axis=None)
-             
-    plt.plot(nu_total_array*HztoGHz, sigma_b_array,'o', lw=7, alpha=1, color='maroon')
-        
+
+    plt.plot(nu_total_array * HztoGHz, sigma_b_array, 'o', lw=7, alpha=1, color='maroon')
+
     plt.xscale('log')
     plt.yscale('log')
-    plt.xlabel('GHz',fontsize=20)
-    plt.ylabel('W/m^2/Hz/Sr',fontsize=20)
+    plt.xlabel('GHz', fontsize=20)
+    plt.ylabel('W/m^2/Hz/Sr', fontsize=20)
 
-    #Make xticks match as best as possible
-    plt.xticks(np.rint(np.logspace(np.log10(80), np.log10(1e3),num=9)),np.rint(np.logspace(np.log10(80), np.log10(1e3),num=9)))
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', frameon=True, prop={'size':12}, ncol=1, title= '{} hour obs.'.format(time/3600))
+    # Make xticks match as best as possible
+    plt.xticks(np.rint(np.logspace(np.log10(80), np.log10(1e3), num=9)),
+               np.rint(np.logspace(np.log10(80), np.log10(1e3), num=9)))
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', frameon=True, prop={'size': 12}, ncol=1,
+               title='{} hour obs.'.format(time / 3600))
     plt.show()
     return None
-    
+
+
 # Function to plot contour plots of run
-def Contour_Plot_Projection(run_number, y_value, electron_temperature, peculiar_velocity, a_sides=1, b_sides=1):
-        
-    # Read simulation output, change directory 
-    df = pd.read_csv('/bolocam/bolocam/erapaport/new_runs/run_{}'.format(run_number),header=None) 
+def contour_plot_projection(run_number, y_value, electron_temperature, peculiar_velocity, a_sides=1, b_sides=1,
+                            cmb_anis=1e-4):
+    """
+
+    """
+
+    # Read simulation output, change directory
+    df = pd.read_csv('/bolocam/bolocam/erapaport/new_runs/run_{}'.format(run_number), header=None)
     data = df.to_numpy()
-            
-    labels = ('y','temperature','peculiar_velocity','a_sides','b_sides')
-    theta = (y_value, electron_temperature, peculiar_velocity, a_sides, b_sides)
 
-    #Plot contour plot
+    labels = ('y', 'temperature', 'peculiar_velocity', 'a_sides', 'b_sides', 'CMB')
+    theta = (y_value, electron_temperature, peculiar_velocity, a_sides, b_sides, cmb_anis)
+
+    # Plot contour plot
     fig = corner.corner(
-    data, labels=labels, truths=theta, smooth = 0
-    );
-        
+        data, labels=labels, truths=theta, smooth=1
+    )
+
     return fig, data
-    
+
+
 # Function to plot comparisons of 2 runs
-def Contour_Plot_Double_Projection(run_number1, run_number2, y_value, electron_temperature, peculiar_velocity, a_sides=1, b_sides=1):
-    
-    # Read simulation output, change directory 
-    df1 = pd.read_csv('/bolocam/bolocam/erapaport/new_runs/run_{}'.format(run_number1),header=None) 
+def contour_plot_double_projection(run_number1, run_number2, y_value, electron_temperature, peculiar_velocity,
+                                   a_sides=1, b_sides=1):
+    """
+
+    """
+
+    # Read simulation output, change directory
+    df1 = pd.read_csv('/bolocam/bolocam/erapaport/new_runs/run_{}'.format(run_number1), header=None)
     data1 = df1.to_numpy()
-        
-    df2 = pd.read_csv('/bolocam/bolocam/erapaport/new_runs/run_{}'.format(run_number2),header=None) 
+
+    df2 = pd.read_csv('/bolocam/bolocam/erapaport/new_runs/run_{}'.format(run_number2), header=None)
     data2 = df2.to_numpy()
-        
+
     chainLabels = ["Run {}".format(run_number1),
-           "Run {}".format(run_number2)]
-        
-    labels = ('y','temperature','peculiar_vel','a_sides','b_sides')
+                   "Run {}".format(run_number2)]
+
+    labels = ('y', 'temperature', 'peculiar_vel', 'a_sides', 'b_sides')
     theta = (y_value, electron_temperature, peculiar_velocity, a_sides, b_sides)
 
-    #Plot contour plot
-    GTC = pygtc.plotGTC(chains=[data1,data2], chainLabels=chainLabels, truths=theta, paramNames = labels,figureSize=10)
-        
+    # Plot contour plot
+    GTC = pygtc.plotGTC(chains=[data1, data2], chainLabels=chainLabels, truths=theta, paramNames=labels, figureSize=10)
+
     return GTC, data1, data2
 
-def Chain_Projection(run_number, y_value, electron_temperature, peculiar_velocity, a_sides=1, b_sides=1):
-        
-    # Read simulation output, change directory 
-    df = pd.read_csv('/bolocam/bolocam/erapaport/new_runs/run_{}'.format(run_number),header=None) 
+
+def chain_projection(run_number, y_value, electron_temperature, peculiar_velocity, a_sides=1, b_sides=1):
+    """
+
+    """
+
+    # Read simulation output, change directory
+    df = pd.read_csv('/bolocam/bolocam/erapaport/new_runs/run_{}'.format(run_number), header=None)
     data = df.to_numpy()
-        
-    labels = ('y','temperature','peculiar_velocity','a_sides','b_sides')
-    theta = (y_value, electron_temperature, peculiar_velocity, a_sides, b_sides)
-        
-    fig, axes = plt.subplots(5, figsize=(30,40), sharex=True)
+
+    labels = ('y', 'temperature', 'peculiar_velocity', 'a_sides', 'b_sides')
+
+    fig, axes = plt.subplots(5, figsize=(30, 40), sharex=True)
     ndim = 5
     for i in range(ndim):
         ax = axes[i]
-        ax.plot(data[:,i], "k", alpha=0.3)
+        ax.plot(data[:, i], "k", alpha=0.3)
         ax.set_ylabel(labels[i])
         ax.yaxis.set_label_coords(-0.1, 0.5)
-            
+
     return fig, axes

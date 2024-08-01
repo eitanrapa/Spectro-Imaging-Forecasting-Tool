69b4ecfc462190e7d88c1493e22552f6599f0e7b
Eitan Rapaport <eitanrapa@gmail.com>
2024-08-01 16:52:31 -0700

    Works with preloaded SZpack so far

--- pkg/sift/ext/SZpack.py
+++ pkg/sift/ext/SZpack.py
@@ -7,6 +7,11 @@
 
 import sift
 
+import sys
+
+sys.path.append("/home/bolocam/erapaport/SIFT_old/codes/SZpack.v1.1.1/python")
+import SZpack as SZ
+
 
 class SZpack:
     """
@@ -16,7 +21,7 @@ class SZpack:
     def __init__(self, tau, temperature, peculiar_velocity, omega=0, sigma=0, kappa=0, betac2_perp=0):
         self.tau = tau
         self.TeSZ = temperature
-        self.betac_para = peculiar_velocity
+        self.betac_para = peculiar_velocity / 3e5
         self.omega = omega
         self.sigma = sigma
         self.kappa = kappa
@@ -27,9 +32,13 @@ class SZpack:
 
         """
 
-        output = sift.ext.sift.szpack_combo_means(xo=xo, tau=self.tau, TeSZ=self.TeSZ, betac_para=self.betac_para,
-                                                  omega=self.omega, sigma=self.sigma, kappa=self.kappa,
-                                                  betac2_perp=self.betac2_perp)
+        # output = sift.ext.sift.szpack_combo_means(xo=xo, tau=self.tau, TeSZ=self.TeSZ, betac_para=self.betac_para,
+        #                                           omega=self.omega, sigma=self.sigma, kappa=self.kappa,
+        #                                           betac2_perp=self.betac2_perp)
+
+        output = SZ.compute_combo_means(xo, self.tau, self.TeSZ, self.betac_para, self.omega, self.sigma, self.kappa,
+                                        self.betac2_perp)
+
         return output
 
 # end of file
